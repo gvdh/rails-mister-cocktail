ded99405a40a5c0b2b73a986235e89c893db10f9
G VDH <vanderhaegen.gauthier@gmail.com>
2017-11-23 15:12:52 +0100

    back end works

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,29 @@
+class DosesController < ApplicationController
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(params_dose)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to @dose.cocktail
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+  end
+
+  private
+
+  def params_dose
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+
+end
