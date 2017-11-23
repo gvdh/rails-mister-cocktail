4c2e169034309758b3ec197f5a97485c1e5c3629
G VDH <vanderhaegen.gauthier@gmail.com>
2017-11-23 18:16:49 +0100

    deploy

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     resources :doses, only: [:new, :create]
   end
   resources :doses, only: [:destroy]
-  root to: "cocktails#index"
+  root to: "cocktails#home"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
