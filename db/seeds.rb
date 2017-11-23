ffe5229ef254e1f5d3c2a2a89012d79261a12fd3
G VDH <vanderhaegen.gauthier@gmail.com>
2017-11-23 16:23:22 +0100

    pas cool

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,23 @@
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.create(name: "Absolut")
+Ingredient.create(name: "Polar Ice")
+Ingredient.create(name: "Danzka")
+Ingredient.create(name: "Frïs")
+Ingredient.create(name: "Grey Goose")
+Ingredient.create(name: "Jean-Marc XO")
+Ingredient.create(name: "Monte-Carlo")
+Ingredient.create(name: "Perfect 1864")
+Ingredient.create(name: "Russian Standard")
+Ingredient.create(name: "Stolichnaya")
+Ingredient.create(name: "Sobieski")
+Ingredient.create(name: "After Diner")
+Ingredient.create(name: "Café Rica")
+Ingredient.create(name: "Cubay")
+Ingredient.create(name: "Caffè Borghetti ")
+Ingredient.create(name: "Kahlua")
+Ingredient.create(name: "Kamok")
+Ingredient.create(name: "Kamora")
+Ingredient.create(name: "Marie Brizard")
+Ingredient.create(name: "Mokatika")
+Ingredient.create(name: "Monin")
+Ingredient.create(name: "lait")
+Ingredient.create(name: "Creme fraiche")
