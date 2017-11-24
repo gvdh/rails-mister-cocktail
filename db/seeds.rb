d54062140589538e1828c10c6fd4ffb592ce5b5b
G VDH <vanderhaegen.gauthier@gmail.com>
2017-11-24 13:38:36 +0100

    trop cool des nouveaux ingredients

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,14 @@
 Cocktail.destroy_all
 Ingredient.destroy_all
 
-Ingredient.create(name: "Absolut")
-Ingredient.create(name: "Polar Ice")
-Ingredient.create(name: "Danzka")
-Ingredient.create(name: "Frïs")
-Ingredient.create(name: "Grey Goose")
-Ingredient.create(name: "Jean-Marc XO")
-Ingredient.create(name: "Monte-Carlo")
-Ingredient.create(name: "Perfect 1864")
-Ingredient.create(name: "Russian Standard")
-Ingredient.create(name: "Stolichnaya")
-Ingredient.create(name: "Sobieski")
-Ingredient.create(name: "After Diner")
-Ingredient.create(name: "Café Rica")
-Ingredient.create(name: "Cubay")
-Ingredient.create(name: "Caffè Borghetti ")
-Ingredient.create(name: "Kahlua")
-Ingredient.create(name: "Kamok")
-Ingredient.create(name: "Kamora")
-Ingredient.create(name: "Marie Brizard")
-Ingredient.create(name: "Mokatika")
-Ingredient.create(name: "Monin")
-Ingredient.create(name: "lait")
-Ingredient.create(name: "Creme fraiche")
+require ‘json’
+require ‘open-uri’
+
+url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+user_serialized = open(url).read
+ingredients = JSON.parse(user_serialized)
+
+
+ingredients['drinks'].each do |element|
+   Ingredient.create(name: element[‘strIngredient1’])
+end
