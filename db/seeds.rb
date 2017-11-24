843ac2a8533d4d25d5764e1ed83a64c1eee87afa
G VDH <vanderhaegen.gauthier@gmail.com>
2017-11-24 14:09:02 +0100

    seeds pas bon trololo

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 Cocktail.destroy_all
 Ingredient.destroy_all
 
-require ‘json’
-require ‘open-uri’
+require 'json'
+require 'open-uri'
 
 url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
 user_serialized = open(url).read
@@ -10,5 +10,5 @@ ingredients = JSON.parse(user_serialized)
 
 
 ingredients['drinks'].each do |element|
-   Ingredient.create(name: element[‘strIngredient1’])
+   Ingredient.create!(name: element['strIngredient1'])
 end
